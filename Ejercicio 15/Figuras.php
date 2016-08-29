de52cb6995ee4264677f77dff61f1529662f4c25
pabloprog3 <pablo_h_n@yahoo.com.ar>
2016-08-29 20:56:00 -0300

    borrar 2 archivos y renombrar Triangulo.php

--- Ejercicio 15/Figuras.php
+++ Ejercicio 15/Figuras.php
@@ -35,10 +35,53 @@ abstract class FiguraGeometrica
 
 	public function toString()
 	{
-
+		
 	}
 
 }
+
+/**
+* 
+*/
+class Triangulo extends FiguraGeometrica
+{
+
+	private $_altura;
+	private $_base;
+	
+	function __construct($b, $h)
+	{
+		$this->_altura = $h;
+		$this->_base = $b;
+	}
+
+class Rectangulo extends FiguraGeometrica
+
+{
+
+	private $_ladoUno;
+	private $_ladoDos;
+
+
+	
+	function __construct($l1, $l2)
+	{
+		$this->_ladoUno = $l1;
+		$this->_ladoDos = $l2;
+	}
+
+
+
+}
+
+	
+
+
+
+
+
+}
+
 
 
 
