6755ec3b7914dc2296b88590843476b5f29a503b
pabloprog3 <pablo_h_n@yahoo.com.ar>
2016-10-17 19:25:44 -0300

    practica 1

---
+++ 17-10-2016/index.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+
+
+
+	<title>MySQL</title>
+</head>
+<body>
+	<h2 id="titulo">Primera clase de MySQL</h2>
+
+	<div class="container">
+
+
+	</div>
+
+</body>
+</html>
+
